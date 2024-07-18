2326f8cb833d9edec8783b083542ab374ab7c422
eric manosalva <ramses.manosalva@gmail.com>
2024-07-17 20:20:22 -0500

    fix: errors on build with images

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,10 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+    image: {
+        service: {
+            entrypoint: 'astro/assets/services/noop'
+        }
+    }
 });
